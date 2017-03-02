e73f59b8dbd4a7cb68c8f2c40e00f5a8eda31756
rebekahlow-jy <rebekahlow.jy@u.nus.edu>
2017-03-02 09:39:56 +0800

    Changed storageStruct to correct json format

--- Firmware/Firmware_Python/Firmware_RPi.py
+++ Firmware/Firmware_Python/Firmware_RPi.py
@@ -2,14 +2,15 @@
 
 import serial
 import time
+import json
 
-serial1 = serial.Serial('/dev/serial0',9600,timeout=1)
+serial1 = serial.Serial('/dev/serial0',9600,timeout=0.01)
 
 initFlag = True
 
 while initFlag:
     msg = "h"
-    serial1.write(msg)
+	serial1.write(msg)
     msg = serial1.read()
     print(msg)
     if (msg == "a"):
@@ -21,4 +22,15 @@ while initFlag:
 
 while True:
     readings = serial1.readline()
-    print (readings)
+	if readings:
+		msg ='n'
+		print (readings)
+		json0 = json.loads(readings)
+		checksumPi = 0;
+		for data in json0['dir']:
+			checksumPi = (checksumPi + data) % 256
+			if (checksumPi ==json0['checksum']):
+				msg = 'a'
+			print (json0['checksum'])
+			print (checksumPi)
+			serial1.write(msg)
