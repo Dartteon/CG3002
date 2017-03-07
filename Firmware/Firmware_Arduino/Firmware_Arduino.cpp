eae81c94cbbef1e926d4814b8134c1593790fba4
rebekahlow-jy <rebekahlow.jy@u.nus.edu>
2017-03-07 11:56:00 +0800

    Renamed variables and packet structure, etc

--- Firmware/Firmware_Arduino/Firmware_Arduino.cpp
+++ Firmware/Firmware_Arduino/Firmware_Arduino.cpp
@@ -12,22 +12,24 @@
 #define MAX_STORAGE_SIZE 100
 #define MAX_SENDING_PACKET_SIZE 10
 
-typedef struct storageStrt {
+typedef struct stepCountStorageStrt {
 	int dir[MAX_STORAGE_SIZE];
 	int accelx[MAX_STORAGE_SIZE];
 	int accely[MAX_STORAGE_SIZE];
 	int accelz[MAX_STORAGE_SIZE];
-} Storage;
+	int timestamp[MAX_STORAGE_SIZE];
+} StepCountStorage;
 
-typedef struct distStorageStrt {
+typedef struct obstDetectionStrt {
 	int dist1[MAX_STORAGE_SIZE]; //Distance from Sensor 1 (Left Arm)
 	int dist2[MAX_STORAGE_SIZE]; //Distance from Sensor 2 (Wand)
 	int dist3[MAX_STORAGE_SIZE]; //Distance from Sensor 3 (Right Arm)
-} DistStorage;
+	int timestamp[MAX_STORAGE_SIZE];
+} ObstDetectionStorage;
 
 
-Storage mainStorage; //Will probably rename another time to dirAccelStorage
-DistStorage distStorage;
+StepCountStorage stepCountStorage;
+ObstDetectionStorage obstDetectionStorage;
 
 int pos_packet = 0;
 int pos_json = 0;
@@ -38,9 +40,9 @@ QueueHandle_t xQueue = NULL;
 
 /*
  void vprint(void *p){
- xSemaphoreTake(xSemaphorePrint, portMAX_DELAY);
- Serial.println(*((String*) p));
- xSemaphoreGive(xSemaphorePrint);
+	 xSemaphoreTake(xSemaphorePrint, portMAX_DELAY);
+	 Serial.println(*((String*) p));
+	 xSemaphoreGive(xSemaphorePrint);
  }
  */
 
@@ -69,20 +71,22 @@ static void initialize() {
 	}
 }
 
-void addData(void *p) {
+void addStepCountDataToStorage(void *p) {
 	int dir1 = 1;
 	int accelx1 = 0;
 	int accely1 = 0;
 	int accelz1 = 0;
+	int timestamp1 = 0;
 
 	for (;;) {
 
-		//Serial.println("addData");
+		//Serial.println("addStepCountData");
 
-		mainStorage.dir[pos_packet] = dir1++;
-		mainStorage.accelx[pos_packet] = accelx1;
-		mainStorage.accely[pos_packet] = accely1;
-		mainStorage.accelz[pos_packet] = accelz1;
+		stepCountStorage.dir[pos_packet] = dir1++;
+		stepCountStorage.accelx[pos_packet] = accelx1++;
+		stepCountStorage.accely[pos_packet] = accely1++;
+		stepCountStorage.accelz[pos_packet] = accelz1++;
+		stepCountStorage.timestamp[pos_packet] = timestamp1++;
 
 		pos_packet = (pos_packet + 1) % MAX_STORAGE_SIZE;
 
@@ -91,18 +95,21 @@ void addData(void *p) {
 
 }
 
-void addDistanceData(void *p) {
+void addObstDetectionDataToStorage(void *p) {
 	//dataUnit temp = *((dataUnit*) p);
 	int dist1 = 1;
 	int dist2 = 1;
 	int dist3 = 0;
+	int timestamp1 = 0;
 
 	for (;;) {
-		distStorage.dist1[pos_packet] = dist1++;
-		distStorage.dist2[pos_packet] = dist2;
-		distStorage.dist3[pos_packet] = dist3;
 
-		//Serial.println(newPacket.id[counter]);
+		//Serial.println("addObstDetectionData");
+
+		obstDetectionStorage.dist1[pos_packet] = dist1++;
+		obstDetectionStorage.dist2[pos_packet] = dist2++;
+		obstDetectionStorage.dist3[pos_packet] = dist3++;
+		obstDetectionStorage.timestamp[pos_packet] = timestamp1++;
 
 		pos_packet = (pos_packet + 1) % MAX_STORAGE_SIZE;
 
@@ -112,27 +119,29 @@ void addDistanceData(void *p) {
 }
 
 
-void transmit(void *p) {
+void transmitStepCountData(void *p) {
 	int i = 0;
 	for (;;) {
 		xSemaphoreTake(xSemaphore, portMAX_DELAY);
 		int pos_backup = pos_json;
 		Serial.println("sendData");
 
-		StaticJsonBuffer<256> jsonBuffer;
+		StaticJsonBuffer<512> jsonBuffer;
 		JsonObject& json = jsonBuffer.createObject();
 		JsonArray& dir = json.createNestedArray("dir");
 		JsonArray& accelx = json.createNestedArray("accelx");
 		JsonArray& accely = json.createNestedArray("accely");
 		JsonArray& accelz = json.createNestedArray("accelz");
+		JsonArray& timestamp = json.createNestedArray("timestamp");
 		int checksum = 0;
 
 		for (i = 0; (pos_json != pos_packet) && (i < MAX_SENDING_PACKET_SIZE); i++) {
-			dir.add(mainStorage.dir[pos_json]);
-			accelx.add(mainStorage.accelx[pos_json]);
-			accely.add(mainStorage.accely[pos_json]);
-			accelz.add(mainStorage.accelz[pos_json]);
-			checksum = (checksum + mainStorage.dir[pos_json]) %256;
+			dir.add(stepCountStorage.dir[pos_json]);
+			accelx.add(stepCountStorage.accelx[pos_json]);
+			accely.add(stepCountStorage.accely[pos_json]);
+			accelz.add(stepCountStorage.accelz[pos_json]);
+			timestamp.add(stepCountStorage.timestamp[pos_json]);
+			checksum = (checksum + stepCountStorage.dir[pos_json]) %256;
 			pos_json = (pos_json + 1) % MAX_STORAGE_SIZE;
 
 		}
@@ -166,6 +175,7 @@ void transmit(void *p) {
 
 }
 
+
 void setup() {
 	xSemaphore = xSemaphoreCreateBinary();
 	xSemaphoreGive(xSemaphore);
@@ -180,10 +190,9 @@ void setup() {
 	Serial.println("begin");
 	initialize();
 	Serial.println("begin2");
-//test
 
-	xTaskCreate(addData, "addData", STACK_SIZE, NULL, 1, NULL);
-	xTaskCreate(transmit, "sendData", STACK_SIZE, NULL, 2, NULL);
+	xTaskCreate(addStepCountDataToStorage, "addStepCountData", STACK_SIZE, NULL, 1, NULL);
+	xTaskCreate(transmitStepCountData, "sendStepCountData", STACK_SIZE, NULL, 2, NULL);
 }
 
 void loop() {
