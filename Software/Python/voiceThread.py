f6acafa3aeba7b836d98f5a5e6c191a13a326cfb
chooxy <chooxy@users.noreply.github.com>
2017-03-21 22:28:18 +0800

    Implement INSTRUCTION class
    Implemented INSTRUCTION class with comparable priority so that priority queue can be used to sort instructions for text-to-speech.

--- Software/Python/voiceThread.py
+++ Software/Python/voiceThread.py
@@ -5,55 +5,72 @@ import subprocess
 # import signal
 import os
 from Queue import Queue
+from Queue import PriorityQueue
 
 ttsTemplate = "espeak -s150 '{msg}' 2>/dev/null"
 
+class INSTRUCTION:
+    '''Used instead of string to make comparable for priority queue'''
+    def __init__(self, message, p):
+        '''Initialise instruction'''
+        self.message = str(message)
+        self.p = int(p)
+        
+    def __lt__(self, other):
+        '''Return true if self instruction has smaller p than other instruction'''
+        return self.p < other.p
+
+    def __eq__(self, other):
+        '''Return true if both instructions have the same p'''
+        return self.p == other.p
+	
 class VoiceHandler:
-	def __init__(self):
-		self.voiceLock = threading.Lock()
-		self.voiceQueue = Queue()
-		self.lastProcess = None
-		self.previousMessage = ''
+    def __init__(self):
+        self.voiceLock = threading.Lock()
+#         self.voiceQueue = Queue()
+        self.voiceQueue = PriorityQueue()
+        self.lastProcess = None
+        self.previousMessage = ''
 
-	#########################
-	# Main voice loop
-	#########################
-	def voiceLoop(self):
-		while True:
-			self.voiceLock.acquire()
-			if self.voiceQueue.empty():
-				pass
-			elif self.lastProcess is None:
-				message = self.voiceQueue.get()
-				self.sayMessage(message)
-			elif self.lastProcess is not None and self.isProcessDone():
-				message = self.voiceQueue.get()
-				self.sayMessage(message)
-			self.voiceLock.release()
-			time.sleep(3)
+    #########################
+    # Main voice loop
+    #########################
+    def voiceLoop(self):
+        while True:
+            self.voiceLock.acquire()
+            if self.voiceQueue.empty():
+                pass
+            elif self.lastProcess is None:
+                message = self.voiceQueue.get().message
+                self.sayMessage(message)
+            elif self.lastProcess is not None and self.isProcessDone():
+                message = self.voiceQueue.get().message
+                self.sayMessage(message)
+            self.voiceLock.release()
+            time.sleep(3)
 
-	#########################
-	# Helper functions
-	#########################
-	def addToQueue(self, message):
-		self.voiceLock.acquire()
-		if self.voiceQueue.empty():
-			print 'queue empty.'
-			self.voiceQueue.put(message)
-			self.previousMessage = message
-		else:
-			self.voiceQueue.put(message)
-			self.previousMessage = message
-		self.voiceLock.release()
+    #########################
+    # Helper functions
+    #########################
+    def addToQueue(self, instruction):
+        self.voiceLock.acquire()
+        if self.voiceQueue.empty():
+            print 'queue empty.'
+            self.voiceQueue.put(instruction)
+            self.previousMessage = instruction
+        else:
+            self.voiceQueue.put(instruction)
+            self.previousMessage = instruction
+        self.voiceLock.release()
 
-	def sayMessage(self, message):
-		print 'Voice output: ' + message
-		voiceCmd = ttsTemplate.format(msg = message)
-		self.lastProcess = subprocess.Popen(voiceCmd, shell=True, stdout=subprocess.PIPE, preexec_fn=os.setsid)
+    def sayMessage(self, message):
+        print 'Voice output: ' + message
+        voiceCmd = ttsTemplate.format(msg = message)
+        self.lastProcess = subprocess.Popen(voiceCmd, shell=True, stdout=subprocess.PIPE, preexec_fn=os.setsid)
 
-	def isProcessDone(self):
-		response = self.lastProcess.poll()
-		if response is None:
-			return False
-		else:
-			return True
+    def isProcessDone(self):
+        response = self.lastProcess.poll()
+        if response is None:
+            return False
+        else:
+            return True
