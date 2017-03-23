f7182c85473aa016a84cdb1080dce6e9ee88fe61
Chris Raynaldo <chris.raynaldo@gmail.com>
2017-03-23 15:25:24 +0800

    Add fraction steps when it's less than stride length
    Add constants.py, change STRIDE_LENGTH to 60.0 to force float

---
+++ Software/Python/constants.py
@@ -0,0 +1,11 @@
+SPEECH_DELAY = 0.5
+STRIDE_LENGTH = 60.0 # Stride length in cm
+
+####################
+# PRIORITY CONSTANTS
+####################
+HIGH_PRIORITY = 0
+MED_PRIORITY = 1
+LOW_PRIORITY = 2
+LOWEST_PRIORITY = 3
+USELESS = 9999
