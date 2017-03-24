08b4aa13e80ee4a6441726471ccfc0e11fe63856
Irwin <irwin.mah@gmail.com>
2017-03-24 12:15:52 +0800

    Modified Input.py to provide feedback If it works as intended, it will loop and convert from input to audio when enter key press is detected. Terminates when '0000' is entered.

--- Software/Python/Input.py
+++ Software/Python/Input.py
@@ -1,10 +1,22 @@
 import sys
 import select
+import os
 
-def getInput():
-    i,o,e = select.select([sys.stdin],[],[]) # No timeout, function returns input after enter is registered.
+def test_function():
+    testString = ''
+    while testString is not '0000':
+        testString = get_input()
+        text_to_speech(testString)
+
+def get_input():
+    i,o,e = select.select([sys.stdin],[],[], 0) # No timeout, function returns input after enter is registered.
     for s in i:
         if s == sys.stdin:
             input = sys.stdin.readline()
             return str(input)
     return e
+
+def text_to_speech(text):
+    os.system("espeak -s 200 -v en+f3 '{msg}' 2>/dev/null".format(msg = text))
+
+test_function()
