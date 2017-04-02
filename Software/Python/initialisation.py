59fc690621bc81bc78e8a8395935ae742c99e87d
Irwin <irwin.mah@gmail.com>
2017-04-02 17:37:36 +0800

    Fix initialisation module

--- Software/Python/initialisation.py
+++ Software/Python/initialisation.py
@@ -1,13 +1,16 @@
+# from gpio import Keypad
+# keypad = Keypad()
 def initialise():
     pass
 
-def get_confirmation(buildingNameOrNumber, floorNumber):
+def get_confirmation(buildingNameOrNumber, floorNumber, nodeRaw):
     while True:
-        voiceOutput.addToQueue(INSTRUCTION(messages.INPUT_CONFIRMATION.format(building = buildingNameOrNumber, floor = floorNumber), constants.HIGH_PRIORITY))
+        voiceOutput.addToQueue(INSTRUCTION(messages.INPUT_CONFIRMATION.format(building = buildingNameOrNumber, floor = floorNumber, node = nodeRaw), constants.HIGH_PRIORITY))
         print('1 to confirm, 2 to try again: ')
         confirmation = str(keypad.getKeysInput())
         voiceOutput.addToQueue(INSTRUCTION(confirmation, constants.HIGH_PRIORITY))
-        if confirmation not ('1' or '2'):
-            voiceOutput.addToQueue(INSTRUCTION(messages.INPUT_OUT_OF_RANGE, constants.HIGH_PRIORITY))
-        else:
+        if confirmation == '1' or confirmation == '2':
             return confirmation
+            # pass
+        else:
+            voiceOutput.addToQueue(INSTRUCTION(messages.INPUT_OUT_OF_RANGE, constants.HIGH_PRIORITY))
