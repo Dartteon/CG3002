de514e27ab97d21978cc9b8b6279991e4382ed69
Irwin <irwin.mah@gmail.com>
2017-04-07 23:47:26 +0800

    Move url into constants and reorganise file.

--- Software/Python/constants.py
+++ Software/Python/constants.py
@@ -1,3 +1,7 @@
+################
+#VALUE CONSTANTS
+################
+
 SPEECH_DELAY = 0.5
 STRIDE_LENGTH = 60.0 # Stride length in cm
 
@@ -9,4 +13,13 @@ MED_PRIORITY = 1
 LOW_PRIORITY = 2
 LOWEST_PRIORITY = 3
 USELESS = 9999
-IS_DEBUG_MODE = False
+
+##################
+#BOOLEAN CONSTANTS
+##################
+IS_DEBUG_MODE = True
+
+#################
+#STRING CONSTANTS
+#################
+BASE_URL = 'http://showmyway.comp.nus.edu.sg/getMapInfo.php?Building={building}&Level={level}'
