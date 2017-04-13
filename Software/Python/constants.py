1cf13a172045870e6ba3102f3157f7ad6a593186
Irwin <irwin.mah@gmail.com>
2017-04-13 16:58:17 +0800

    Minor change for format

--- Software/Python/constants.py
+++ Software/Python/constants.py
@@ -19,21 +19,22 @@ USELESS = 9999
 
 class PRIORITIES:
     numberOf = 6
-    # HIGHEST PRIORITY
     (
+    # HIGHEST PRIORITY
     INIT,
     DESTINATION,
     NODE,
     STEP_HIGH,
     TURN,
     STEP_LOW,
-    ) = range(numberOf)
     # LOWEST PRIORITY
+    ) = range(numberOf)
+
 
 ##################
 #BOOLEAN CONSTANTS
 ##################
-IS_DEBUG_MODE = False
+IS_DEBUG_MODE = True
 
 #################
 #STRING CONSTANTS
